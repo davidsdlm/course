b1989c1bb38cc7d1fe7458d80ae4c8431c2c9d99
davidsdlm <da.hayrapetyan.99.2@gmail.com>
2023-04-10 14:25:43 +0700

    gunicorn

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ URL = "https://lab.karpov.courses/hardml-api/module-5/get_secret_number"
 @app.route('/return_secret_number')
 @requests_counter
 def hello_world():
+    time.sleep(1)
     return jsonify(secret_number=secret_number)
 
 
